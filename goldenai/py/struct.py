89db57d3af69f4c397403e932253f9eec68218db
Guangyu He <guangyu.he@golden-tech.de>
2025-08-11 11:34:00 +0200

    feat(struct): add input and output adjustments for GPT-5 and GPT-5 Nano models

--- goldenai/py/struct.py
+++ goldenai/py/struct.py
@@ -21,6 +21,12 @@ class GoldenAIParsedResponse(ParsedResponse):
         elif "gpt-4.1" in self.model:
             input = 1.7
             output = 6.84
+        elif "gpt-5-nano" in self.model:
+            input = 0.05
+            output = 0.4
+        elif "gpt-5" in self.model:
+            input = 1.25
+            output = 10
         else:
             raise Exception("Unsupported model: " + self.model)
 
